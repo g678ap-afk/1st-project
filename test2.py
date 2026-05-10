4138885d5a52b47e58a0e8a5553e755c903eebaf
g678ap-afk <g678ap@gmail.com>
2026-05-10 13:05:04 +0200

    homeworkostak

--- test2.py
+++ test2.py
@@ -1,6 +1,21 @@
-name = input("Введи ім'я: ")
-surname = input("Введи прізвище: ")
-age = input("Скільки тобі років: ")
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.money = 50
+        self.progress = 0
 
-print(f"Привіт мене звати {name} {surname}. Мені {age}!")
+    def live(self):
+        for day in range(1, 366):
+            if self.money < 20:
+                self.money += 50  # Працює
+            elif self.progress < 10:
+                self.progress += 5  # Вчиться
+                self.money -= 5
+            else:
+                self.money -= 10  # Відпочиває
 
+            if self.money < 0: break  # Перевірка на банкрутство
+
+
+s = Student("Олег")
+s.live()
